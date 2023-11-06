e21605ecd55ab8f68eb7fef2753e3d61b39fa666
hg <15074685969@163.com>
2023-11-06 17:08:27 +0800

    dev

--- main.go
+++ main.go
@@ -14,12 +14,35 @@ import (
 )
 
 type TicketType struct {
-	Brand     string `yaml:"brand"`
-	SeatType  string `yaml:"seatType"`
-	TicketID  string `yaml:"ticketId"`
-	Cookie    string `yaml:"cookie"`
-	StartTime string `yaml:"startTime"`
-	Num       string `yaml:"num"`
+	Brand                string  `yaml:"brand"`
+	SeatType             string  `yaml:"seatType"`
+	TicketID             string  `yaml:"ticketId"`
+	Cookie               string  `yaml:"cookie"`
+	StartTime            string  `yaml:"startTime"`
+	AddressID            string  `json:"AddressID"`
+	GoodsAmount          float64 `json:"goods_amount"`
+	ShippingFee          int     `json:"shipping_fee"`
+	GoodsID              int     `json:"goods_id"`
+	AttrID               int     `json:"attr_id"`
+	Num                  int     `json:"num"`
+	SumPayPrice          float64 `json:"sumPayPrice"`
+	IsInv                bool    `json:"is_inv"`
+	LgsID                int     `json:"lgs_id"`
+	Integral             int     `json:"integral"`
+	InvoiceType          int     `json:"invoice_type"`
+	InvoiceTitle         string  `json:"invoice_title"`
+	InvoicePrice         float64 `json:"invoice_price"`
+	CompanyName          string  `json:"CompanyName"`
+	TaxpayerID           string  `json:"TaxpayerID"`
+	CompanyAddress       string  `json:"CompanyAddress"`
+	CompanyPhone         string  `json:"CompanyPhone"`
+	CompanyBankOfDeposit string  `json:"CompanyBankOfDeposit"`
+	CompanyBankNo        string  `json:"CompanyBankNo"`
+	RuleGoodslistContent string  `json:"rule_goodslist_content"`
+	RadomRuleGoodslist   string  `json:"radom_rule_goodslist_content"`
+	Remark               string  `json:"remark"`
+	IsIntegralOffset     bool    `json:"IsIntegralOffsetFreight"`
+	R                    float64 `json:"r"`
 }
 
 func main() {
@@ -55,7 +78,7 @@ func main() {
 	maxConcurrentRequests := 5
 
 	// 创建通道用于通知抢票结果
-	ticketChan := make(chan bool, 5)
+	ticketChan := make(chan bool, maxConcurrentRequests)
 
 	// 创建等待组
 	var wg sync.WaitGroup
@@ -67,7 +90,8 @@ func main() {
 		// 启动最大并发请求数的抢票任务
 		for i := 0; i < maxConcurrentRequests; i++ {
 			wg.Add(1)
-			go ticketAdd(ticket, ticketChan, &wg)
+			//go ticketAdd(ticket, ticketChan, &wg)
+			go goodsAdd(ticket, ticketChan, &wg)
 		}
 
 		// 等待所有抢票任务完成
@@ -89,6 +113,63 @@ func main() {
 		}
 	}
 	close(ticketChan)
+}
+
+func goodsAdd(ticket TicketType, ticketChan chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	url := "https://shop.48.cn/Order/BuySaveForGive"
+	method := "POST"
+
+	payload := fmt.Sprintf("AddressID=%s&goods_amount=%.4f&shipping_fee=%d&goods_id=%d&attr_id=%d&num=%d&sumPayPrice=%.2f&is_inv=%t&lgs_id=%d&integral=%d&invoice_type=%d&invoice_title=%s&invoice_price=%.2f&CompanyName=%s&TaxpayerID=%s&CompanyAddress=%s&CompanyPhone=%s&CompanyBankOfDeposit=%s&CompanyBankNo=%s&rule_goodslist_content=%s&radom_rule_goodslist_content=%s&remark=%s&IsIntegralOffsetFreight=%t&r=%.16f",
+		ticket.AddressID, ticket.GoodsAmount, ticket.ShippingFee, ticket.GoodsID, ticket.AttrID, ticket.Num, ticket.SumPayPrice, ticket.IsInv, ticket.LgsID, ticket.Integral, ticket.InvoiceType, ticket.InvoiceTitle, ticket.InvoicePrice, ticket.CompanyName, ticket.TaxpayerID, ticket.CompanyAddress, ticket.CompanyPhone, ticket.CompanyBankOfDeposit, ticket.CompanyBankNo, ticket.RuleGoodslistContent, ticket.RadomRuleGoodslist, ticket.Remark, ticket.IsIntegralOffset, ticket.R)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, url, bytes.NewBufferString(payload))
+
+	if err != nil {
+		log.Println(err)
+		ticketChan <- false
+		return
+	}
+	req.Header.Add("Accept", "application/json, text/javascript, */*; q=0.01")
+	req.Header.Add("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Add("Cookie", ticket.Cookie)
+	req.Header.Add("Origin", "https://shop.48.cn")
+	req.Header.Add("Referer", "https://shop.48.cn/order/buy")
+	req.Header.Add("Sec-Fetch-Dest", "empty")
+	req.Header.Add("Sec-Fetch-Mode", "cors")
+	req.Header.Add("Sec-Fetch-Site", "same-origin")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36")
+	req.Header.Add("X-Requested-With", "XMLHttpRequest")
+	req.Header.Add("sec-ch-ua", "\"Chromium\";v=\"118\", \"Google Chrome\";v=\"118\", \"Not=A?Brand\";v=\"99\"")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", "\"Linux\"")
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		ticketChan <- false
+		return
+	}
+	var bodyMessage Message
+	err = json.Unmarshal(body, &bodyMessage)
+	if err != nil {
+		log.Println(err)
+		ticketChan <- false
+		return
+	}
+	log.Println(bodyMessage)
+	if bodyMessage.HasError == false {
+		ticketChan <- true
+		return
+	}
+
+	ticketChan <- false
+	return
 }
 
 func ticketAdd(ticket TicketType, ticketChan chan bool, wg *sync.WaitGroup) {

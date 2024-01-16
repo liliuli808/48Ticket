af540e4fc2720c6ed72eb0dbf0df2ab52ba337f4
hg <15074685969@163.com>
2024-01-16 19:28:57 +0800

    feat

---
+++ login/login.go
@@ -0,0 +1,121 @@
+package login
+
+import (
+	"awesomeProject/model"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+func QuickLogin(ticket model.TicketType) string {
+	// 目标登录URL
+	url := "https://user.48.cn/QuickLogin/login/"
+
+	// 构造登录表单数据
+
+	payload := strings.NewReader(fmt.Sprintf("phone=&phonecode=&login_type=&area=&preg=&username=%s&password=%s", ticket.Username, ticket.Password))
+
+	req, err := http.NewRequest("POST", url, payload)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return ""
+	}
+	req.Header.Add("Accept", "application/json, text/javascript, */*; q=0.01")
+	req.Header.Add("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+	req.Header.Add("Cache-Control", "no-cache")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Add("Origin", "https://user.48.cn")
+	req.Header.Add("Pragma", "no-cache")
+	req.Header.Add("Referer", "https://user.48.cn/Login/index.html?return_url=https://shop.48.cn/home/index")
+	req.Header.Add("Sec-Fetch-Dest", "empty")
+	req.Header.Add("Sec-Fetch-Mode", "cors")
+	req.Header.Add("Sec-Fetch-Site", "same-origin")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+	req.Header.Add("X-Requested-With", "XMLHttpRequest")
+	req.Header.Add("sec-ch-ua", `"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"`)
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", `"Linux"`)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	// 读取响应内容
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return ""
+	}
+
+	var response Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Println(err)
+	}
+
+	srcContent := extractSrcContent(response.Desc)
+	cookiesStr := ""
+	for _, v := range srcContent {
+		req, err := http.NewRequest("GET", v, nil)
+		if err != nil {
+			fmt.Println("Error creating request:", err)
+			return ""
+		}
+		req.Header.Add("Accept", "application/json, text/javascript, */*; q=0.01")
+		req.Header.Add("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+		req.Header.Add("Cache-Control", "no-cache")
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+		req.Header.Add("Origin", "https://user.48.cn")
+		req.Header.Add("Pragma", "no-cache")
+		req.Header.Add("Referer", "https://user.48.cn/Login/index.html?return_url=https://shop.48.cn/home/index")
+		req.Header.Add("Sec-Fetch-Dest", "empty")
+		req.Header.Add("Sec-Fetch-Mode", "cors")
+		req.Header.Add("Sec-Fetch-Site", "same-origin")
+		req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+		req.Header.Add("X-Requested-With", "XMLHttpRequest")
+		req.Header.Add("sec-ch-ua", `"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"`)
+		req.Header.Add("sec-ch-ua-mobile", "?0")
+		req.Header.Add("sec-ch-ua-platform", `"Linux"`)
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+
+		cookies := resp.Cookies()
+		for _, cookie := range cookies {
+			if cookie.Name != ".AspNet.ApplicationCookie" {
+				continue
+			}
+			cookiesStr += fmt.Sprintf("  %s=%s;", cookie.Name, cookie.Value, cookie.Domain)
+		}
+	}
+	return cookiesStr
+}
+
+func extractSrcContent(input string) []string {
+	// Regular expression pattern to match script tags and capture src attribute
+	scriptPattern := `<script[^>]*\s+src="([^"]+)"[^>]*>`
+
+	// Compile the regular expression
+	re := regexp.MustCompile(scriptPattern)
+
+	// Find all matches in the input
+	matches := re.FindAllStringSubmatch(input, -1)
+
+	// Extract content from the captured group
+	var srcContents []string
+	for _, match := range matches {
+		if len(match) == 2 {
+			srcContents = append(srcContents, match[1])
+		}
+	}
+
+	return srcContents
+}
+
+type Response struct {
+	Status int    `json:"status"`
+	Desc   string `json:"desc"`
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"awesomeProject/login"
+	"awesomeProject/model"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -9,45 +11,14 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"sync"
 	"time"
 )
 
-type TicketType struct {
-	Brand                string  `yaml:"brand"`
-	SeatType             string  `yaml:"seatType"`
-	TicketID             string  `yaml:"ticketId"`
-	Cookie               string  `yaml:"cookie"`
-	StartTime            string  `yaml:"startTime"`
-	AddressID            string  `json:"AddressID"`
-	GoodsAmount          float64 `json:"goods_amount"`
-	ShippingFee          int     `json:"shipping_fee"`
-	GoodsID              int     `json:"goods_id"`
-	AttrID               int     `json:"attr_id"`
-	Num                  string  `json:"num"`
-	SumPayPrice          float64 `json:"sumPayPrice"`
-	IsInv                bool    `json:"is_inv"`
-	LgsID                int     `json:"lgs_id"`
-	Integral             int     `json:"integral"`
-	InvoiceType          int     `json:"invoice_type"`
-	InvoiceTitle         string  `json:"invoice_title"`
-	InvoicePrice         float64 `json:"invoice_price"`
-	CompanyName          string  `json:"CompanyName"`
-	TaxpayerID           string  `json:"TaxpayerID"`
-	CompanyAddress       string  `json:"CompanyAddress"`
-	CompanyPhone         string  `json:"CompanyPhone"`
-	CompanyBankOfDeposit string  `json:"CompanyBankOfDeposit"`
-	CompanyBankNo        string  `json:"CompanyBankNo"`
-	RuleGoodslistContent string  `json:"rule_goodslist_content"`
-	RadomRuleGoodslist   string  `json:"radom_rule_goodslist_content"`
-	Remark               string  `json:"remark"`
-	IsIntegralOffset     bool    `json:"IsIntegralOffsetFreight"`
-	R                    float64 `json:"r"`
-}
-
 func main() {
+
+	// 创建或打开一个日志文件
 	logFile, err := os.OpenFile("log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("无法打开日志文件:", err)
@@ -57,8 +28,6 @@ func main() {
 	// 设置日志输出到文件
 	log.SetOutput(logFile)
 
-	var cstZone = time.FixedZone("CST", 8*3600) // 东八
-	time.Local = cstZone
 	// 读取YAML配置文件
 	yamlFile, err := os.ReadFile("./config.yaml")
 	if err != nil {
@@ -66,141 +35,112 @@ func main() {
 	}
 
 	// 创建TicketType结构体实例
-	var ticket TicketType
+	var ticket model.TicketType
 
 	// 解析YAML配置文件到结构体
 	err = yaml.Unmarshal(yamlFile, &ticket)
 	if err != nil {
 		log.Fatalf("无法解析YAML文件: %v", err)
 	}
-
-	// 定义日期时间字符串的格式
-	format := "2006-01-02 15:04:05"
-
-	// 使用 time 包中的 Parse 函数将字符串解析为时间对象
-	dateTime, err := time.ParseInLocation(format, ticket.StartTime, cstZone)
-	if err != nil {
-		fmt.Println("解析日期时间失败:", err)
-		return
-	}
-	log.Println(dateTime.String())
-	log.Println(time.Now())
-	// 设置最大并发请求数
-	maxConcurrentRequests := 5
+	ticket.Cookie = login.QuickLogin(ticket)
+	//// 设置最大并发请求数
+	maxConcurrentRequests := 3
 
 	// 创建通道用于通知抢票结果
-	ticketChan := make(chan bool, maxConcurrentRequests)
-
-	// 创建等待组
-	var wg sync.WaitGroup
 	for {
-		currentTime := time.Now().Add(3 * time.Second)
+		format := "2006-01-02 15:04:05"
+
+		var cstZone = time.FixedZone("CST", 8*3600) // 东八
+		time.Local = cstZone
+
+		// 使用 time 包中的 Parse 函数将字符串解析为时间对象
+		dateTime, err := time.ParseInLocation(format, ticket.StartTime, cstZone)
+		if err != nil {
+			fmt.Println("解析日期时间失败:", err)
+			continue
+		}
+		currentTime := time.Now().Add(20 * time.Millisecond)
+		// 如果当前时间早于抢票时间，则等待
 		if currentTime.Before(dateTime) {
 			continue
 		}
+		var wg sync.WaitGroup
 		// 启动最大并发请求数的抢票任务
 		for i := 0; i < maxConcurrentRequests; i++ {
 			wg.Add(1)
-			go ticketAdd(ticket, ticketChan, &wg)
+			go func() {
+				ticketAdd(ticket)
+				defer wg.Done()
+			}()
 		}
-
-		// 等待所有抢票任务完成
 		wg.Wait()
-
-		// 统计抢票结果
-		successCount := 0
-		for i := 0; i < maxConcurrentRequests; i++ {
-			if <-ticketChan {
-				successCount++
-			}
-		}
-
-		if successCount > 0 {
-			log.Println("抢票成功")
-			break
-		} else {
-			log.Println("抢票失败，继续尝试...")
-		}
+		time.Sleep(time.Second)
 	}
-	close(ticketChan)
+
 }
 
-func ticketAdd(ticket TicketType, ticketChan chan bool, wg *sync.WaitGroup) {
-	defer wg.Done()
-
+func ticketAdd(ticket model.TicketType) bool {
+	log.Println("开始抢票")
 	requestData := "ticketsid=%s&num=%s&seattype=%s&brand_id=%s&choose_times_end=-1&ticketstype=2&r=0.056981472084815854"
 	requestURL := "https://shop.48.cn/TOrder/ticket_Add"
 
 	requestData = fmt.Sprintf(requestData, ticket.TicketID, ticket.Num, ticket.SeatType, ticket.Brand)
 
-	proxyURL, err := url.Parse("http://liliuli808:woai258123@tunnel1.docip.net:13541")
-	if err != nil {
-		fmt.Println("Error parsing proxy URL:", err)
-		os.Exit(1)
-	}
-	transport := &http.Transport{
-		Proxy: http.ProxyURL(proxyURL),
-	}
 	// 创建一个HTTP请求客户端
-	client := &http.Client{
-		Transport: transport,
-		Timeout:   time.Second,
-	}
+	client := &http.Client{}
 
 	// 创建一个HTTP POST请求
-	req, err := http.NewRequest("POST", requestURL, bytes.NewBufferString(requestData))
+	request, err := http.NewRequest("POST", requestURL, bytes.NewBufferString(requestData))
 	if err != nil {
 		log.Println(err)
-		ticketChan <- false
-		return
+		return false
 	}
 
 	// 设置请求头
-	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
-	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-	req.Header.Set("Cookie", ticket.Cookie)
-	req.Header.Set("Origin", "https://shop.48.cn")
-	req.Header.Set("Referer", "https://shop.48.cn/TOrder")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-	req.Header.Set("sec-ch-ua", `"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"`)
-	req.Header.Set("sec-ch-ua-mobile", "?0")
-	req.Header.Set("sec-ch-ua-platform", `"Linux"`)
+	request.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
+	request.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+	request.Header.Set("Cache-Control", "no-cache")
+	request.Header.Set("Connection", "keep-alive")
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	request.Header.Set("Cookie", ticket.Cookie)
+	request.Header.Set("Origin", "https://shop.48.cn")
+	request.Header.Set("Pragma", "no-cache")
+	request.Header.Set("Referer", fmt.Sprintf("https://shop.48.cn/tickets/item/%s?seat_type=%s", ticket.TicketID, ticket.SeatType))
+	request.Header.Set("Sec-Fetch-Dest", "empty")
+	request.Header.Set("Sec-Fetch-Mode", "cors")
+	request.Header.Set("Sec-Fetch-Site", "same-origin")
+	request.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+	request.Header.Set("X-Requested-With", "XMLHttpRequest")
+	request.Header.Set("sec-ch-ua", `"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"`)
+	request.Header.Set("sec-ch-ua-mobile", "?0")
+	request.Header.Set("sec-ch-ua-platform", `"Linux"`)
 
 	// 发送HTTP请求
-	resp, err := client.Do(req)
+	resp, err := client.Do(request)
 	if err != nil {
 		log.Println(err)
-		ticketChan <- false
-		return
+		return false
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
-		ticketChan <- false
-		return
+		return false
 	}
 	var bodyMessage Message
 	err = json.Unmarshal(body, &bodyMessage)
 	if err != nil {
 		log.Println(err)
-		ticketChan <- false
-		return
+		return false
 	}
-	log.Println(bodyMessage)
-	if bodyMessage.HasError == false {
-		ticketChan <- true
-		return
+	log.Println(string(body))
+	if bodyMessage.HasError == true {
+		return false
 	}
-
-	ticketChan <- false
-	return
+	if bodyMessage.Message == "success" {
+		return true
+	}
+	return false
 }
 
 type Message struct {
